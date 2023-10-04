f4d3edfe7fe565ad35a38c9c840105255d677427
Harold Tomlinson <haroldt@creacom.ca>
2023-10-03 22:01:51 -0400

    initial generated swift project.

---
+++ Dedup/Dedup/DedupApp.swift
@@ -0,0 +1,17 @@
+//
+//  DedupApp.swift
+//  Dedup
+//
+//  Created by Harold Tomlinson on 2023-10-03.
+//
+
+import SwiftUI
+
+@main
+struct DedupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
