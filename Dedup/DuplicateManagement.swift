d18be569c0dfd5a4e80d4988254bb154f2c3c199
Harold Tomlinson <haroldt@creacom.ca>
2023-11-03 23:52:52 -0400

    Added dialog with the details for the button pressed. The dialog shows only the data for the first button.  Need to fix the data passed to the dialog.

---
+++ Dedup/DuplicateManagement.swift
@@ -0,0 +1,18 @@
+//
+//  DuplicateManagement.swift
+//  Dedup
+//
+//  Created by Harold Tomlinson on 2023-10-28.
+//
+
+import SwiftUI
+
+struct DuplicateManagement: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DuplicateManagement()
+}
